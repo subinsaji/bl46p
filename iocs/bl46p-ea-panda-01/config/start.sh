1d3a4e34f8b7279ef84b439f9fb90a4a03b1b4a8
Subin Saji <subinssaji@gmail.com>
2024-01-08 12:03:23 +0000

    panda ioc name 02 to 01

--- iocs/bl46p-ea-panda-01/config/start.sh
+++ iocs/bl46p-ea-panda-01/config/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p /epics/opi
 
-pandablocks-ioc softioc bl46p-mo-panda-02 BL46P-EA-PANDA-02 --clear-bobfiles --screens-dir /epics/opis
+pandablocks-ioc softioc bl46p-mo-panda-01 BL46P-EA-PANDA-01 --clear-bobfiles --screens-dir /epics/opis
 
 
